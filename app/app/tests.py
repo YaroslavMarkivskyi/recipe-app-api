f26832cabf8c9a6b05606bc03fcf67edc62b8beb
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2024-01-30 15:01:56 +0200

    Added psycopg2

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample tests"""
+
+from django.test import TestCase
+
+from app import calc 
+
+
+class CalcTests(TestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test Athat two numbers are added together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
